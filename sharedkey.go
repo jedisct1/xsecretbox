e4cd0192305ba2b3be1362c269977157f197ef6b
Frank Denis <github@pureftpd.org>
2019-09-09 17:53:34 +0200

    Revert "Replace x/crypto/curve25519 with cloudflare/circl/dh/x25519"
    This reverts commit c1d4ff3cf35cc6c03124fcbcaabbcae47857f30e.

--- sharedkey.go
+++ sharedkey.go
@@ -4,19 +4,20 @@ import (
 	"errors"
 
 	"github.com/aead/chacha20/chacha"
-	"github.com/cloudflare/circl/dh/x25519"
+	"golang.org/x/crypto/curve25519"
 )
 
 // SharedKey computes a shared secret compatible with the one used by `crypto_box_xchacha20poly1305``
 func SharedKey(secretKey [32]byte, publicKey [32]byte) ([32]byte, error) {
 	var sharedKey [32]byte
-	var cfSharedKey, cfSecretKey, cfPublicKey x25519.Key
-	copy(cfSecretKey[:], secretKey[:])
-	copy(cfPublicKey[:], publicKey[:])
-	if ! x25519.Shared(&cfSharedKey, &cfSecretKey, &cfPublicKey)  {
+	curve25519.ScalarMult(&sharedKey, &secretKey, &publicKey)
+	c := byte(0)
+	for i := 0; i < 32; i++ {
+		c |= sharedKey[i]
+	}
+	if c == 0 {
 		return sharedKey, errors.New("weak public key")
 	}
-	copy(sharedKey[:], cfSharedKey[:])
 	var nonce [16]byte
 	chacha.HChaCha20(&sharedKey, &nonce, &sharedKey)
 	return sharedKey, nil
