8c186358e888fe5f774b4f9523a4c93951e7a725
Frank Denis <github@pureftpd.org>
2019-09-09 17:53:24 +0200

    Revert "Format"
    This reverts commit f55b79ae1214c3cd276cf906d8c3e1db3e4da014.

--- sharedkey.go
+++ sharedkey.go
@@ -7,21 +7,23 @@ import (
 	"github.com/cloudflare/circl/dh/x25519"
 )
 
+	var zeroNonce [16]byte
+
 // SharedKey computes a shared secret compatible with the one used by `crypto_box_xchacha20poly1305``
 func SharedKey(secretKey [32]byte, publicKey [32]byte) ([32]byte, error) {
 	var sharedKey [32]byte
 	var cfSharedKey, cfSecretKey, cfPublicKey x25519.Key
 	copy(cfSecretKey[:], secretKey[:])
 	copy(cfPublicKey[:], publicKey[:])
-	if !x25519.Shared(&cfSharedKey, &cfSecretKey, &cfPublicKey) {
+	if ! x25519.Shared(&cfSharedKey, &cfSecretKey, &cfPublicKey)  {
 		return sharedKey, errors.New("weak public key")
 	}
-	HChaCha20(&sharedKey)
+	copy(sharedKey[:], cfSharedKey[:])
+	chacha.HChaCha20(&sharedKey, &zeroNonce, &sharedKey)
 	return sharedKey, nil
 }
 
 // HChaCha20 - Hash the result of an X25519 key exchange in order to get a box-compatible shared secret
 func HChaCha20(sharedKey *[32]byte) {
-	var zeroNonce [16]byte
 	chacha.HChaCha20(sharedKey, &zeroNonce, sharedKey)
 }
